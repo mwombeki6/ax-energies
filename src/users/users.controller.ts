793db6a8ce345f5311743d7bf86403a68cfb0773
mwombeki6 <hackerlubex@protonmail.com>
2025-03-26 23:13:21 +0300

    code refactor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get('profile')
+    @UseGuards(JwtAuthGuard)
+    getProfile(@Request() req) {
+        return { phoneNumber: req.user.phoneNumber, role: req.user.role }
+    }
+}
