b324065d7899fa7c4e34b8c3bcc394dac9e1fc5d
mwombeki6 <hackerlubex@protonmail.com>
2025-04-24 16:41:44 +0300

    added station entity

---
+++ src/station/station.entity.ts
@@ -0,0 +1,47 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '../users/user.entity';
+
+@Entity()
+export class Station {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  address: string;
+
+  @Column({ nullable: true })
+  logoUrl: string;
+
+  @Column({ nullable: true })
+  contactPhone: string;
+
+  @Column({ nullable: true })
+  contactEmail: string;
+
+  @OneToMany(() => Pump, (pump) => pump.station)
+  pumps: Pump[];
+
+  @OneToMany(() => User, (user) => user.station)
+  users: User[];
+
+  @OneToMany(() => FuelInventory, (inventory) => inventory.station)
+  fuelInventories: FuelInventory[];
+
+  @OneToMany(() => Staff, (staff) => staff.station)
+  staff: Staff[];
+
+  @OneToMany(() => Transaction, (transaction) => transaction.station)
+  transactions: Transaction[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
