8b7c86c5de2eb7b3e23719f75a570d9648728edc
mwombeki6 <hackerlubex@protonmail.com>
2025-04-24 18:05:31 +0300

    deleted fuel_inventory module

--- src/fuel_inventory/fuel_inventory.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('fuel-inventory')
-export class FuelInventoryController {}
--- src/fuel_inventory/fuel_inventory.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FuelInventoryController } from './fuel_inventory.controller';
-import { FuelInventoryService } from './fuel_inventory.service';
-
-@Module({
-  controllers: [FuelInventoryController],
-  providers: [FuelInventoryService]
-})
-export class FuelInventoryModule {}
