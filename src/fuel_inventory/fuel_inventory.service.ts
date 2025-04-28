9c6258ecceb753c78eebaca03d33a8b22182039d
mwombeki6 <hackerlubex@protonmail.com>
2025-04-28 07:49:35 +0300

    implemented fuel_inventory and delivery modules

--- src/fuel_inventory/fuel_inventory.service.ts
+++ src/fuel_inventory/fuel_inventory.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FuelInventory } from './fuel_inventory.entity';
+import { Repository } from 'typeorm';
+import { Station } from '../station/station.entity';
+import { FuelType } from '../fuel_type/fuel_type.entity';
+import { CreateFuelInventoryDto } from './dto/fuel_inventory.dto';
 
 @Injectable()
-export class FuelInventoryService {}
+export class FuelInventoryService {
+  constructor(
+    @InjectRepository(FuelInventory)
+    private fuelInventoryRepository: Repository<FuelInventory>,
+    @InjectRepository(Station)
+    private stationRepository: Repository<Station>,
+    @InjectRepository(FuelType)
+    private fuelTypeRepository: Repository<FuelType>,
+  ) {}
+
+  async create(createDto: CreateFuelInventoryDto): Promise<FuelInventory> {}
+}
